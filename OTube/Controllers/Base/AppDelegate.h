c3375dfcb2b25f708d85b7e34fdb66372c3949d1
Quang Phù <phuchiminhquang@gmail.com>
2016-09-23 10:39:48 +0700

    Init project file

---
+++ OTube/Controllers/Base/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OTube
+//
+//  Created by Quang Phù on 9/23/16.
+//  Copyright © 2016 Quang Phu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
